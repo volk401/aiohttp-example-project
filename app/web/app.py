ddfdb48b7324d2e5fc1f2f2e96f9fe71bdace3cd
volk401 <sgfox4@mail.ru>
2023-02-16 23:42:22 +0300

    INIT

---
+++ app/web/app.py
@@ -0,0 +1,33 @@
+from aiohttp_apispec import setup_aiohttp_apispec
+from typing import Optional
+from aiohttp.web import Application as AiohttpApplication, run_app as aiohttp_run_app, View as AiohttpView, \
+    Request as AiohttpRequest
+
+from app.web.routes import setup_routes
+from app.store import setup_accessors
+from app.web.middlewares import setup_middlewares
+
+class Application(AiohttpApplication):
+    database: dict = {}
+
+class Request(AiohttpRequest):
+    @property
+    def app(self) -> Application:
+        return super().app()
+
+
+class View(AiohttpView):
+    @property
+    def request(self) -> Request:
+        return super().request
+
+
+app = Application()
+
+
+def run_app():
+    setup_routes(app)
+    setup_aiohttp_apispec(app, title='CRM Application', url='/docs/json', swagger_path='/docs')
+    setup_middlewares(app)
+    setup_accessors(app)
+    aiohttp_run_app(app)
